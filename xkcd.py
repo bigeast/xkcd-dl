9611a69606dc366c3a737a392f31403d3a8b6c15
Wang dadong <dadongking@gmail.com>
2015-03-26 14:20:33 +0800

    comiled regex.

--- xkcd.py
+++ xkcd.py
@@ -7,12 +7,16 @@ def xkcd_dl(low, high):
     for n in range(low, high + 1):
         a = urllib2.urlopen('https://xkcd.com/' + str(n))
         f = a.read()
-        imgurl, title, alt = re.findall(r'<img src="([^"]*)" title="([^"]*)" alt="([^"]*)"',f)[0]
-        title = HTMLParser().unescape(title)
-        print(str(n).ljust(3), imgurl.ljust(60), alt)
-        # xkcd = open('xkcd' + str(n) + '.html', 'wb')
-        # xkcd.write(f)
-        # xkcd.close()
+        pattern = re.compile(r'<img src="([^"]*)" title="([^"]*)" alt="([^"]*)"')
+        res = pattern.search(f).groups()
+        imgurl = res[0]
+        title = res[1]
+        alt = res[2]
+ #       title = unicode(title)
+ #       title = urllib2.unquote(title)
+ #       title = HTMLParser().unescape(title)
+ #       print title
+        print(str(n).ljust(4), imgurl.ljust(60), alt)
         imgfile = open('xkcd ' + str(n) + ':' + alt + '.jpg', 'wb')
         img = urllib2.urlopen(imgurl).read()
         imgfile.write(img)
@@ -30,6 +34,6 @@ else:
         low = int(sys.argv[1])
         high = low + 10
     else:
-        low = 0
+        low = 1
         high = low + 10
     xkcd_dl(low, high)
