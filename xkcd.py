8828525bd34544dd4ff66f0fc606b08e5b20a2e6
Wang dadong <dadongking@gmail.com>
2015-03-27 22:31:11 +0800

    rewrite using library *lxml*.

--- xkcd.py
+++ xkcd.py
@@ -1,22 +1,34 @@
 #!/bin/env python2
 import urllib2
-import re
-from HTMLParser import HTMLParser
+import lxml.html
 import sys
+import time
 def xkcd_dl(low, high):
-    for n in range(low, high + 1):
-        a = urllib2.urlopen('https://xkcd.com/' + str(n))
-        f = a.read()
-        pattern = re.compile(r'<img src="([^"]*)" title="([^"]*)" alt="([^"]*)"')
-        res = pattern.search(f).groups()
-        imgurl = res[0]
-        title = res[1]
-        alt = res[2]
-        print(str(n).ljust(4), imgurl.ljust(60), alt)
-        imgfile = open('xkcd ' + str(n) + ':' + alt + '.jpg', 'wb')
-        img = urllib2.urlopen(imgurl).read()
-        imgfile.write(img)
-        imgfile.close()
+    all_size = 0
+    all_s = time.time()
+    for n in range(low, high):
+        if n == 404:
+            continue
+        s = time.time()
+        url = 'https://xkcd.com/' + str(n)
+        content = urllib2.urlopen(url)
+        doc_tree = lxml.html.fromstring(content.read())
+        img = doc_tree.xpath('//img')[1]
+        comic_url = img.attrib['src']
+        comic_title = img.attrib['alt']
+        comic_declare = img.attrib['title']
+        comic_file = open('xkcd ' + str(n) + ':' + comic_url.split('/')[-1], 'wb')
+        comic = urllib2.urlopen(comic_url).read()
+        comic_file.write(comic)
+        comic_file.close()
+        t = time.time()
+        file_size = len(comic)
+        all_size += file_size;
+        print(str(n).ljust(4), comic_url.ljust(60), "%.1f" % (file_size / 1024.0) + "K",  comic_title, "%.4f" % (t - s))
+    all_t = time.time()
+    print("%d comics downloaded in %d s, average download time is %.4f s." % (high - low, all_t - all_s, ((all_t - all_s) / (high - low))))
+    all_size_str = "%.1f" % (all_size / 1024.0)  + "K" if all_size < 1024 * 1024 else "%.1f" % (all_size / 1024.0 / 1024.0)  + "M"
+    print("Total size: " + all_size_str);
 
 Usage = "xkcd.py fromID [toID]"
 argc = len(sys.argv)
@@ -33,3 +45,6 @@ else:
         low = 1
         high = low + 10
     xkcd_dl(low, high)
+# xkcd.com/404 doesn't exist.
+# xkcd.com/934 name is 'MAC/PC'.
+# compiled regex seems didn't improve the performance, comic 755~934 take 179s to download.
